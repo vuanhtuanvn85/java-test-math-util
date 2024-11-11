4ecb0cbd6c119b7ded9494e9dc22c288a37490be
Tuan Vu Anh <vuanhtuanvn85@gmail.com>
2024-11-11 14:31:59 +0700

    refactor code to test regression

--- src/com/vutu/mathutil/core/MathUtil.java
+++ src/com/vutu/mathutil/core/MathUtil.java
@@ -19,10 +19,6 @@ public class MathUtil {
         if (n == 0 || n == 1) {
             return 1;
         }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n-1);
     }
 }
